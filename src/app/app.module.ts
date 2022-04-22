280df8de2993c977b4b5be53398a6b36de03f915
Yeminem <omdnrd@gmail.com>
2022-04-22 00:40:04 -0500

    Just trying to actually commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatCardModule } from '@angular/material/card';
+
+import { MatInputModule } from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
-import{ MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,9 +22,17 @@ import{ MatCardModule } from '@angular/material';
     LoginComponent
   ],
   imports: [
+    AppRoutingModule,
+
     BrowserModule,
     BrowserAnimationsModule,
+    
+    MatFormFieldModule,
+    MatInputModule,
     MatCardModule,
+    MatToolbarModule,
+
+
 
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,8 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  goToPage(PageName:string):void{
 
+
+    this.router.navigate([`${PageName}`]);
+    // else
+    // outterrormessage
+  }
   ngOnInit(): void {
   }
 
