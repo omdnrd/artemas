c43fff55c78fecf8833d5e76d2e593e03654cc9a
Yeminem <omdnrd@gmail.com>
2022-04-22 16:10:08 -0500

    Initial Routing Imports

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,4 +1,9 @@
 import { NgModule } from '@angular/core';
+
+import { ExpenseComponent } from '../expense/expense.component';
+import { FamilyComponent } from '../family/family.component';
+import { HomeComponent } from '../home/home.component';
+import { IncomeComponent } from '../income/income.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
